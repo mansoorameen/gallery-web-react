23f4362f6097ca79d878fd9e241427c5d28de39f
mansoorameen <manzooravail@gmail.com>
2021-04-20 14:39:51 +0530

    add: dark mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { useState } from 'react';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
-import Title from './comps/Title';
+// import Title from './comps/Title'; 
 import UploadForm from './comps/UploadForm';
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div``;
 
 function App() {
 
   const [ selectedImg, setSelectedImg ] = useState(null);
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+    <GlobalStyles />
     
-    <div className="App">
-      <Title/>
+    <StyledApp className="App">
+
+    <div className="title">
+      <div>
+      <h1>Gallery</h1> </div>
+      <div className="switch">
+      <input className="toggle" type="checkbox" onClick={() => themeToggler()} />
+      </div>
+
+      <h2>Pictures</h2>
+    </div>
+      {/* <Title/> */}
       <UploadForm />
       <ImageGrid setSelectedImg={setSelectedImg} />
       { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} /> }
-    </div>
+    </StyledApp>
+    </ThemeProvider>
     
   );
 }
