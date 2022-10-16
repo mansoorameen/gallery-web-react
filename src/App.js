103e80e1db8ecad828a4d1dac4377f7f6a46f84e
mansoorameen <manzooravail@gmail.com>
2022-10-16 18:11:40 +0530

    code improv and skeleton div

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import ImageGrid from './comps/ImageGrid';
-import Modal from './comps/Modal';
-import UploadForm from './comps/UploadForm';
+import React, { useEffect, useState } from "react";
+import ImageGrid from "./comps/ImageGrid";
+import Modal from "./comps/Modal";
+import UploadForm from "./comps/UploadForm";
 import styled, { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, GlobalStyles } from "./comps/theme/GlobalStyles";
-import Toggler from './comps/theme/Toggler';
-import ScrollTopIcon from './comps/ScrollTopIcon';
+import {
+  lightTheme,
+  darkTheme,
+  GlobalStyles,
+} from "./comps/theme/GlobalStyles";
+import Toggler from "./comps/theme/Toggler";
+import ScrollTopIcon from "./comps/ScrollTopIcon";
 
 const StyledApp = styled.div``;
 
 function App() {
-
-  const [ selectedImg, setSelectedImg ] = useState(null);
+  const [selectedImg, setSelectedImg] = useState(null);
   const [theme, setTheme] = useState("light");
-  
-  useEffect(()=>{
-    const data = localStorage.getItem('data')
-    
-    if(data){
-      setTheme(JSON.parse(data))
-     }
-    },[])
-    
-    useEffect(()=>{
-      localStorage.setItem('data',JSON.stringify(theme))
-    })
- 
+
+  useEffect(() => {
+    const data = localStorage.getItem("data");
+
+    if (data) {
+      setTheme(JSON.parse(data));
+    }
+  }, []);
+
+  onbeforeunload = (event) =>
+    localStorage.setItem("data", JSON.stringify(theme));
+
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-    <GlobalStyles />
-    
-    <StyledApp className="App">
+      <GlobalStyles />
 
-    <div className="title">
-      <Toggler theme={theme} setTheme={setTheme} />
-      <h1>Gallery</h1>
-      <p>Find what people are sharing</p>
-    </div>
-      <UploadForm />
-      <ScrollTopIcon />
-      <ImageGrid setSelectedImg={setSelectedImg} />
-      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} /> }
-    </StyledApp>
+      <StyledApp className="App">
+        <div className="title">
+          <Toggler theme={theme} setTheme={setTheme} />
+          <h1>Gallery</h1>
+          <p>Find what people are sharing</p>
+        </div>
+        <UploadForm />
+        <ScrollTopIcon />
+        <ImageGrid setSelectedImg={setSelectedImg} theme={theme} />
+        {selectedImg && (
+          <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+        )}
+      </StyledApp>
     </ThemeProvider>
-    
   );
 }
 
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import useFirestore from '../hooks/useFirestore';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import useFirestore from "../hooks/useFirestore";
+import { motion } from "framer-motion";
 
-const ImageGrid = ({ setSelectedImg }) => {
+const ImageGrid = ({ setSelectedImg, theme }) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+  const { docs } = useFirestore("images");
 
-    const { docs } = useFirestore('images');
-
-    return (
-        <div className="img-grid">
-            { docs && docs.map(doc => (
-                <motion.div className="img-wrap" key={doc.id}
-                layout
-                whileHover={{ opacity: 1 }}
-                onClick={() => 
-                    setSelectedImg(doc.url)}>
-
-                <motion.img src={doc.url} alt="uploaded pic"
-                initial={{ opacity: 0}}
-                animate={{ opacity: 1}}
-                transition={{ delay: 1 }} />
-                </motion.div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="img-grid">
+      {docs.length ? (
+        docs.map((doc) => (
+          <>
+            <motion.div
+              className="img-wrap"
+              style={imgLoaded ? { border: `2px solid #F87171` } : {}}
+              key={doc.id}
+              layout
+              whileHover={{ opacity: 1 }}
+              onClick={() => {
+                setSelectedImg(doc.url);
+              }}
+            >
+              <motion.img
+                onLoad={() => setImgLoaded(true)}
+                src={doc.url}
+                alt="uploaded pic"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, height: 10, width: 10 }}
+                transition={{ delay: 1 }}
+              />
+            </motion.div>
+          </>
+        ))
+      ) : (
+        <>
+          <motion.div className="img-wrap">
+            <motion.img className="skeleton" />
+          </motion.div>
+          <motion.div className="img-wrap">
+            <motion.img className="skeleton" />
+          </motion.div>
+          <motion.div className="img-wrap">
+            <motion.img className="skeleton" />
+          </motion.div>
+          <motion.div className="img-wrap">
+            <motion.img className="skeleton" />
+          </motion.div>
+          <motion.div className="img-wrap">
+            <motion.img className="skeleton" />
+          </motion.div>
+          <motion.div className="img-wrap">
+            <motion.img className="skeleton" />
+          </motion.div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default ImageGrid;
